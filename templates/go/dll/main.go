d769ca3812f79b2a0b6b2cf25d20ff0d2befcd3f
lesnuages <1799430+lesnuages@users.noreply.github.com>
2023-12-20 10:46:58 -0800

    Update Go template with argument parser

--- templates/go/dll/main.go
+++ templates/go/dll/main.go
@@ -2,37 +2,51 @@ package main
 
 import "C"
 import (
+	"{{.PackageName}}/pkg/parser"
 	"syscall"
 	"unsafe"
+
 	"{{.PackageName}}/pkg/{{.ExtensionName}}"
 )
 
-func sendOutput(data string, callback uintptr) {
-	// Convert Go string to byte slice pointer
-	outDataPtr, _ := syscall.BytePtrFromString(data)
-
-	// Send data back - discard return/error, yolo
-	syscall.SyscallN(callback, uintptr(unsafe.Pointer(outDataPtr)), uintptr(len(data)))
-}
-
-func sendError(err error, callback uintptr) {
-	sendOutput(err.Error(), callback)
-}
-
-func getArgs(data uintptr, dataLen uintptr) string {
-	// Get args from Pointer
-	argSlice := unsafe.Slice((*byte)(unsafe.Pointer(data)), int(dataLen))
-	return string(argSlice)
-}
-
 // This is the entrypoint called by the Sliver implant at runtime.
 // Arguments are passed in via the `data` parameter as a byte array of size `dataLen`.
-// To send data back to the implant, we recommend using the `sendOutput` helper function.
-// Errors can be sent back using the `sendError` helper.
+// Use the OutputBuffer.SendOutput() and OutputBuffer.SendError() methods to
+// prepare the data to be sent back to the implant.
+// Data is sent with a call to OutputBuffer.Flush().
 
 //export Run
 func Run(data uintptr, dataLen uintptr, callback uintptr) uintptr {
-	output, err := {{.ExtensionName}}.DoStuff(getArgs(data, dataLen))
+	// Prepare the output buffer used to send data back to the implant
+	outBuff := parser.NewOutBuffer(callback)
+	// Create a new argument parser
+	dataParser, err := parser.NewParser(data, dataLen)
+	if err != nil {
+		outBuff.SendError(err)
+		outBuff.Flush()
+	}
+
+	// Parse arguments 
+	
+	// Get a string argument
+	stringArg, err := dataParser.GetString()
+	if err != nil {
+		outBuff.SendError(err)
+		outBuff.Flush()
+	}
+
+	// Get an int argument
+	intArg, err := dataParser.GetInt()
+	if err != nil {
+		outBuff.SendError(err)
+		outBuff.Flush()
+	}
+
+	if intArg > 0 {
+		// do stuff
+	}
+
+	output, err := {{.ExtensionName}}.DoStuff(stringArg)
 	if err != nil {
 		sendError(err, callback)
 		return 1
---
+++ templates/go/pkg/parser/parser.go
@@ -0,0 +1,125 @@
+package parser
+
+import (
+	"encoding/binary"
+	"fmt"
+	"strings"
+	"syscall"
+	"unsafe"
+)
+
+type DataParser struct {
+	original []byte
+	n        int
+}
+
+// NewParser takes a pointer to the data and the length of the data
+// and returns a DataParser object
+func NewParser(data, dataLen uintptr) (*DataParser, error) {
+	if data == 0 || dataLen == 0 {
+		return nil, fmt.Errorf("no data to parse")
+	}
+	//turn uintptrs into slices
+	dp := DataParser{
+		original: unsafe.Slice((*byte)(unsafe.Pointer(data)), dataLen),
+		n:        4,
+	}
+	return &dp, nil
+}
+
+func (dp *DataParser) GetString() (string, error) {
+	outStr, err := dp.GetData()
+	if err != nil {
+		return "", err
+	}
+	return string(outStr), nil
+}
+
+// GetData returns a slice of bytes. The underlying data could be a string, a file, etc.
+func (dp *DataParser) GetData() ([]byte, error) {
+	if dp.GetDataLength() < 4 {
+		return nil, fmt.Errorf("no more data to return")
+	}
+	//extract the length
+	l := binary.LittleEndian.Uint32(dp.original[dp.n:])
+	//increment n
+	dp.n += 4
+	//copy to a new buffer to avoid mutating the underlying state
+	if dp.GetDataLength() < int(l) {
+		return nil, fmt.Errorf("no more data to return")
+	}
+	rb := make([]byte, l)
+	c := copy(rb, dp.original[dp.n:])
+	if c != int(l) {
+		return nil, fmt.Errorf("expected to copy %x but copied %x", l, c)
+	}
+	//increment n
+	dp.n += c
+
+	return rb, nil
+}
+
+// GetInt returns a uint32 from the DataParser
+func (dp *DataParser) GetInt() (uint32, error) {
+	if dp.GetDataLength() < 4 {
+		return 0, fmt.Errorf("no more data to return")
+	}
+	r := binary.LittleEndian.Uint32(dp.original[dp.n:])
+	dp.n += 4
+	return r, nil
+}
+
+// GetShort returns a uint16 from the DataParser
+func (dp *DataParser) GetShort() (uint16, error) {
+	if dp.GetDataLength() < 2 {
+		return 0, fmt.Errorf("no more data to return")
+	}
+	r := binary.LittleEndian.Uint16(dp.original[dp.n:])
+	dp.n += 2
+	return r, nil
+}
+
+// GetDataLength returns the remaining length of unparsed data
+func (dp *DataParser) GetDataLength() int {
+	return len(dp.original) - dp.n
+}
+
+type OutputBuffer struct {
+	b        strings.Builder
+	done     bool
+	callback uintptr
+}
+
+func NewOutBuffer(callback uintptr) *OutputBuffer {
+	return &OutputBuffer{b: strings.Builder{}, callback: callback}
+}
+
+func (o *OutputBuffer) SendOutput(data string) {
+	//errors not captured here, where would we send them?
+	o.b.WriteString(data)
+	o.b.WriteString("\n") //newline
+}
+
+func (o *OutputBuffer) SendError(err error) {
+	o.SendOutput(fmt.Sprintf("error: %s", err.Error()))
+}
+
+func (o *OutputBuffer) Flush() {
+	//write the buffer - checks if it's already been flushed to avoid crashing the process
+	if o.done {
+		//figure out a way to return a message to implant that something went wrong with flush
+		return
+	}
+	_sendOutput(o.b.String(), o.callback)
+	o.done = true
+}
+
+// data should only be sent once per call from implant (for now)
+func _sendOutput(data string, callback uintptr) {
+	outDataPtr, err := syscall.BytePtrFromString(data)
+	if err != nil {
+		return
+	}
+	// Send data back
+	syscall.SyscallN(callback, uintptr(unsafe.Pointer(outDataPtr)), uintptr(len(data)))
+}
