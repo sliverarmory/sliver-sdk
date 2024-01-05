5f030ffd6386c9a84ef28d91f4f57027a7642f0e
lesnuages <1799430+lesnuages@users.noreply.github.com>
2024-01-05 10:09:16 -0800

    Update templates

--- templates/extensions/rust/src/lib.rs
+++ templates/extensions/rust/src/lib.rs
@@ -5,7 +5,7 @@ const SUCCESS: u32 = 0;
 const ERROR: u32 = 1;
 
 #[no_mangle]
-pub extern "C" fn starts(
+pub extern "C" fn start(
     data: *mut u8,
     size: u64,
     result_callback: extern "C" fn(*mut u8, u64),
@@ -22,12 +22,20 @@ pub extern "C" fn starts(
             return ERROR;
         }
     };
+    let second_arg = match dp.get_string() {
+        Ok(v) => v,
+        Err(e) => {
+            ob.send_error(&format!("failed to get second argument: {}", e));
+            ob.flush();
+            return ERROR;
+        }
+    };
     // Append data to output buffer
     ob.send_output(format!("first arg: {}", firt_arg).as_str());
-    ob.send_output(format!("second arg: {}", dp.get_string().unwrap()).as_str());
+    ob.send_output(format!("second arg: {}", second_arg).as_str());
 
     // call do_stuff from myextension.rs
-    let result = match {{.ExtensionName}}::do_stuff(firt_arg, dp.get_string().unwrap().as_str()) {
+    let result = match {{.ExtensionName}}::do_stuff(firt_arg, second_arg.as_str()) {
         Ok(v) => v,
         Err(e) => {
             ob.send_error(&format!("failed to call do_stuff: {}", e));
--- templates/extensions/rust/src/myextension.rs
+++ templates/extensions/rust/src/myextension.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
 pub fn do_stuff(int_arg: u32, str_arg: &str) -> Result<String, Box<dyn Error>> {
-    Ok("Hello World!".to_string())
+    let out = format!("[do_stuff] got {} and {}", int_arg, str_arg);
+    Ok(out)
 }
